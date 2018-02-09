8fcbbd1994663ca09ce41b581c8411a8f150fdbc
wojtekrichert <wojtek.richert1996@gmail.com>
2018-02-09 20:58:44 +0100

    Initial Commit

---
+++ eigen_faces/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eigen_faces
+//
+//  Created by Wojciech Richert on 09.02.2018.
+//  Copyright © 2018 Wojciech Richert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
