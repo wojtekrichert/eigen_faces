83beffdf46201ad9cbdb5a46ef5df9512558e6a4
wojtekrichert <wojtek.richert1996@gmail.com>
2018-02-11 19:15:47 +0100

    TODO:visualisation of final points

--- eigen_faces/main.cpp
+++ eigen_faces/main.cpp
@@ -10,6 +10,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <vector>
+#include <armadillo>
 using namespace cv;
 using namespace std;
 const int liczbazdjec = 20;
@@ -42,7 +43,7 @@ int main() {
         string aaa = "/Users/wojtek/Desktop/eigen_faces/eigen_faces/" +to_string(img_number+1)+".png" ;
         Mat img = imread(aaa, 1);
         cvtColor( img, img, CV_BGR2GRAY );
-        img.convertTo(img, CV_64F);
+        img.convertTo(img, CV_64F,1,0);
         for (int i=0; i<img.rows; ++i)
             array[i] = img.ptr<double>(i);
         int x =0;
@@ -54,15 +55,8 @@ int main() {
             }
         }
     }
-/*
-    // displaying all elements in tablica:
-    for(int i = 0; i <m*n; ++i){
-        for(int j= 0; j<liczbazdjec; ++j){
-            cout << " "<< tablica[i][j];
-        }
-        cout << "\n";
-    }
- */
+
+
     for(int i = 0; i <m*n; ++i){
         avg[i]= int(avg[i]/liczbazdjec);
         //cout << avg[i] << "\n";
@@ -81,15 +75,82 @@ int main() {
             display[j]= tablica[j][img_number];
         }
         Mat A(n, m, CV_64F, display );
-        A.convertTo(A,CV_8U,1,0);
-        namedWindow( "Average face", CV_WINDOW_AUTOSIZE );
-        imshow( "Average face", A );
+        A.convertTo(A,CV_64F,1,0);
+        namedWindow( "faces-Average face", WINDOW_NORMAL);
+        moveWindow("faces-Average face", 0, 0);
+        imshow( "faces-Average face", A );
         waitKey(0);
         
+        
     }
+    
+    /*
+    //svd algorithm:
+    double** transtablica = new double*[liczbazdjec];
+    for(int i = 0; i < liczbazdjec; ++i){
+        transtablica[i] = new double[m*n];
+    }
+    for (int i = 0; i < liczbazdjec; i ++) {
+        for (int j = 0; j < m*n; j++) {
+            tablica[j][i] = transtablica[i][j];
+        }
+    }
+    double** res = new double*[m*n];
+    for(int i = 0; i < m*n; ++i){
+        res[i] = new double[m*n];
+    }
+    for(int i = 0; i < m*n; ++i)
+        for(int j = 0; j < m*n; ++j)
+            for(int k = 0; k < liczbazdjec; ++k)
+            {
+                res[i][j] += tablica[i][k] * transtablica[k][j];
+            }
+     */
+    arma::mat Q(m*n,liczbazdjec);
+    for(int i= 0; i<liczbazdjec;++i){
+        for(int j=0; j<m*n;++j){
+            Q(j,i)=tablica[j][i];
+        }
+    }
+    arma::mat U;
+    arma::vec s;
+    arma::mat V;
+    arma::svd_econ(U, s, V, Q);
+    int x =0;
+    for(int j=0; j<m*n;++j){
+        display[x]= U(j,0)*1000;
+        ++x;
+    }
+    Mat D(n, m, CV_64F, display );
+    D.convertTo(D,CV_64F);
+    namedWindow( "aaa", WINDOW_NORMAL);
+    moveWindow("aaa", 0, 0);
+    imshow( "aaa", D );
+    waitKey(0);
+    
+    arma::mat Y(liczbazdjec,m*n);
+    for(int i= 0; i<liczbazdjec;++i){
+        for(int j=0; j<m*n;++j){
+            Y(i,j)=tablica[j][i];
+        }
+    }
+    
+    arma:: mat Z(m*n,3);
+    for(int j=0; j<m*n;++j){
+        for(int i =0; i<3; ++i){
+            Z(j,i)= U(j,i)*1000;
+        }
+    }
+    
+    arma::mat points(3,liczbazdjec);
+    points= Y*Z;
+    cout<<arma::size(points);
+    cout << points;
     delete [] display;
     delete [] array;
     delete [] tablica;
     delete [] avg;
+    //delete [] transtablica;
+    //delete [] res;
 }
 
